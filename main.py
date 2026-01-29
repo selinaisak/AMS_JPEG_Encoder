5ca6e02d8c3149a5564d309a7e29d1542ca6279e
Selina Isak <seisak@edu.aau.at>
2026-01-30 00:38:19 +0100

    Define basic steps of JPEG encoder

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# This is a basic JPEG encoder
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+###### GENERAL STEPS OF A JPEG ENCODER ######
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('JPEG')
+    # Color space conversion RGB --> YCbCr
+    # Chroma subsampling (4:2:0)
+    # Block preparation/splitting (8x8)
+    # Shift pixel value range [0, 255] → [-128, 127] (for DCT)
+    # Direct Cosine Transform (DCT)
+    # Quantization (quantization table/matrix!)
+    # Zigzag scan/ordering
+    # Differential encoding (DC)
+    # Run-length Encoding (AC)
+    # Huffman Encoding (Huffman tables!)
+    # Frame builder --> construct/display JPEG encoded image!
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
