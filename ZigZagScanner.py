0d0f5bdae95c676253bb3cefb68446f71bce9598
Selina Isak <seisak@edu.aau.at>
2026-02-06 00:54:52 +0100

    Modified printing + forced input data to be int32

--- ZigZagScanner.py
+++ ZigZagScanner.py
@@ -1,7 +1,13 @@
 import numpy as np
 
 class ZigZagScanner:
+    # Perform the zigzag scan for all channels
     def zigzag_all_blocks(self, Y_blocks, Cb_blocks, Cr_blocks):
+        # Force block values to be integers
+        Y_blocks = Y_blocks.astype(np.int32)
+        Cb_blocks = Cb_blocks.astype(np.int32)
+        Cr_blocks = Cr_blocks.astype(np.int32)
+
         for channel_blocks in (Y_blocks, Cb_blocks, Cr_blocks):
             for blocks in channel_blocks:
                 for block in blocks:
@@ -10,7 +16,6 @@ class ZigZagScanner:
     # Perform the zigzag scan: Go up, if the diagonal has an even index, and
     # go down, if it has an odd index --> keep direction changes in mind at all times!
     def __zigzag_block(self, block):
-
         if(block.shape[0] != block.shape[1]):
             raise ValueError(f"Block size is not square! {block.shape}")
 
@@ -28,7 +33,7 @@ class ZigZagScanner:
                         # traverse from bottom-left to top-right --> swap indices!
                         # the bigger i(column) --> the smaller j(row) --> approaching top-right-corner
                         scan.append(block[j, i])
-                        print(f"EVEN i={j}, j={i}")
+                        #print(f"EVEN i={j}, j={i}")
             else:
                 # odd diagonals go top-down
                 for i in range(diagonal + 1):
@@ -37,9 +42,9 @@ class ZigZagScanner:
                         # traverse from top-right to bottom-left --> keep indices in order!
                         # the bigger i(row) --> the smaller j(column) --> approaching bottom-left-corner
                         scan.append(block[i, j])
-                        print(f"ODD i={i}, j={j}")
+                        #print(f"ODD i={i}, j={j}")
 
-        print(f"Block: {block.shape}, Scan: {len(scan)}")
+        print(f"Scan_shape: {len(scan)}, Scan: {np.array(scan).flatten().tolist()}")
         return scan
 
 
